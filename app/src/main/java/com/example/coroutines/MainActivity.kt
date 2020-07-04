01e4a21ae1b3bf63558b2c07caf9d3b89a72193c
edmayen <ed_mayen@hotmail.com>
2020-07-04 15:03:43 -0500

    Coroutine Contexts

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.coroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,14 +25,35 @@ class MainActivity : AppCompatActivity() {
 
 
         //Suspend Functions
+        /*
         GlobalScope.launch {
             val networkCallAnswer = doNetworkCall()
             val networkCallAnswer2 = doNetworkCall2()
             Log.d(TAG, networkCallAnswer)
             Log.d(TAG, networkCallAnswer2)
         }
+         */
+
+        //Coroutines Context
+        GlobalScope.launch(Dispatchers.IO){
+            Log.d(TAG, "Starting coroutine in thread ${Thread.currentThread().name}")
+            val answer = doNetworkCall()
+            withContext(Dispatchers.Main){
+                Log.d(TAG, "Setting text in thread ${Thread.currentThread().name}")
+                tvDummy.text = answer
+            }
+        }
+
     }
 
+    //Coroutines Context
+    suspend fun doNetworkCall(): String{
+        delay(30000L)
+        return "This is the answer"
+    }
+
+    //Suspend Functions
+    /*
     suspend fun doNetworkCall(): String{
         delay(30000L)
         return "This is the answer"
@@ -43,4 +63,5 @@ class MainActivity : AppCompatActivity() {
         delay(30000L)
         return "This is the answer"
     }
+     */
 }
