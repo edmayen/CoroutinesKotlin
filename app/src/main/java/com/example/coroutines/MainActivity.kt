1c6b65f6d4b8ef61b9a069ceeec0c3570e40bddf
edmayen <ed_mayen@hotmail.com>
2020-07-04 14:51:43 -0500

    Suspend Functions

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -15,10 +15,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // First Coroutine
+        /*
         GlobalScope.launch{
-            delay((3000L))
-            Log.d(TAG, "Coroutine says hello from thread ${Thread.currentThread().name}")
+           delay((3000L))
+           Log.d(TAG, "Coroutine says hello from thread ${Thread.currentThread().name}")
         }
         Log.d(TAG, "Hello from thread ${Thread.currentThread().name}")
+         */
+
+
+        //Suspend Functions
+        GlobalScope.launch {
+            val networkCallAnswer = doNetworkCall()
+            val networkCallAnswer2 = doNetworkCall2()
+            Log.d(TAG, networkCallAnswer)
+            Log.d(TAG, networkCallAnswer2)
+        }
+    }
+
+    suspend fun doNetworkCall(): String{
+        delay(30000L)
+        return "This is the answer"
+    }
+
+    suspend fun doNetworkCall2(): String{
+        delay(30000L)
+        return "This is the answer"
     }
 }
